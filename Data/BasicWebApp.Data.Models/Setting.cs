30d7f65a3bb3a329db6de68ee3d8e29b4e6f6868
DinyoD <40523718+DinyoD@users.noreply.github.com>
2021-08-27 15:25:53 +0300

    Initial Create Niki Kostov template.

---
+++ Data/BasicWebApp.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicWebApp.Data.Models
+{
+    using BasicWebApp.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
